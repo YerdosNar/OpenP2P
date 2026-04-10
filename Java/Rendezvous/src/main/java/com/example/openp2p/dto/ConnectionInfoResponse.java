88b1d366408efa4130a7c259e2cad23a3b531707
YerdosNar <besway2000@gmail.com>
2026-04-10 22:07:01 +0900

    I will redo in Java later

--- Java/Rendezvous/src/main/java/com/example/openp2p/dto/ConnectionInfoResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.openp2p.dto;
-
-/**
- * ConnectionInfoResponse
- */
-public record ConnectionInfoResponse(String ip, int port) {}
--- Java/Rendezvous/src/main/java/com/example/openp2p/dto/RegisterHostRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.openp2p.dto;
-
-/**
- * RegisterHostRequest
- */
-public record RegisterHostRequest(String hostId, String password, int hostPort) {}
